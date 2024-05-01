ca7a442056a270f6ea889a6c000dbda60c6be1ed
Marcos Lucas <marcosvaldeni@gmail.com>
2024-05-01 20:23:46 +0100

    Creation and listing routes (HTTP Methods)

--- src/server.js
+++ src/server.js
@@ -1,6 +1,16 @@
 import http from 'node:http';
 
 const server = http.createServer((req, res) => {
+  const { method, url } = req;
+
+  if (method === 'GET' && url === '/users') {
+    return res.end('Users listing');
+  }
+
+  if (method === 'POST' && url === '/users') {
+    return res.end('User creation');
+  }
+
   return res.end('Hello World');
 });
 
