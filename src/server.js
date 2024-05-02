69b704a75373193d36e56de7f9e169b608b55fd5
Marcos Lucas <marcosvaldeni@gmail.com>
2024-05-02 07:12:30 +0100

    Knowing HTTP status codes

--- src/server.js
+++ src/server.js
@@ -16,10 +16,10 @@ const server = http.createServer((req, res) => {
       name: 'John Doe',
       email: 'johndoe@example.com'
     });
-    return res.end('User creation');
+    return res.writeHead(201).end();
   }
 
-  return res.end('Hello World');
+  return res.writeHead(404).end();
 });
 
 server.listen(3333);
