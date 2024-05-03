aecdc2005af9fd82d87803587f91594dabe49852
Marcos Lucas <marcosvaldeni@gmail.com>
2024-05-03 18:20:38 +0100

    Consumindo uma stream completa

--- streams/stream-http-server.js
+++ streams/stream-http-server.js
@@ -11,10 +11,18 @@ class InverseNumberString extends Transform {
   }
 }
 
-const server = http.createServer((req, res) => {
-  return req
-    .pipe(new InverseNumberString())
-    .pipe(res);
+const server = http.createServer(async (req, res) => {
+  const buffers = [];
+
+  for await (const chunk of req) {
+    buffers.push(chunk)
+  }
+
+  const fullStreamContent = Buffer.concat(buffers).toString();
+
+  console.log(fullStreamContent);
+
+  return res.end(fullStreamContent);
 
 });
 
